8257f5f6836c330eda7ffad5c78d472e1d9b9096
James Browning <jbxmv@mail.missouri.edu>
2017-02-14 11:25:38 -0500

    Work on VC

--- SimpleToDoList/ViewController.swift
+++ SimpleToDoList/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newItem: UIButton!
+    @IBOutlet weak var addItem: UIButton!
+    
+    
+    @IBAction func newList(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
