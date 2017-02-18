24b4a28fd60d2257b3002a780dbdef83689fcc2d
James Browning <jbxmv@mail.missouri.edu>
2017-02-18 00:07:49 -0500

    VC to turn in.

--- SimpleToDoList/ViewController.swift
+++ SimpleToDoList/ViewController.swift
@@ -16,13 +16,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var quantityTextField: UITextField!
     
+    @IBOutlet weak var list: UILabel!
+    var listItems : String = ""
+    
+    @IBAction func descExit(_ sender: UITextField) {
+        descriptionTextField.resignFirstResponder()
+    }
+    
+    @IBAction func quanExit(_ sender: UITextField) {
+        quantityTextField.resignFirstResponder()
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func buttonClick(sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Error", message: "Insert an integer value in the quantity text field.", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "Close Alert", style: .default, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func newList(_ sender: UIButton) {
+        descriptionTextField.resignFirstResponder()
+        quantityTextField.resignFirstResponder()
+        listItems = ""
+        descriptionTextField.text=""
+        quantityTextField.text=""
+        list.text = listItems
         newItem.isEnabled = true
         addItem.isEnabled = true
         descriptionTextField.isEnabled = true
         quantityTextField.isEnabled = true
     }
     
+    @IBAction func addNewItem(_ sender: UIButton) {
+        descriptionTextField.text = ""
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+
+        if nil != quantityTextField {
+            if let quantity = Int(quantityTextField.text!) {
+                if nil != descriptionTextField
+                    && descriptionTextField.text != ""
+                    && quantityTextField.text != "" {
+                    listItems.append("\(descriptionTextField.text!) x \(quantity)\n")
+                }
+            } else {
+                buttonClick(sender: sender)
+            }
+        }
+        list.text=listItems
+        descriptionTextField.text=""
+        quantityTextField.text=""
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
